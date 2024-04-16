59bbfaec6a6d0b4050f571fff9abcd5a97aec026
PaolaCriss <133990388+PaolaCriss@users.noreply.github.com>
2024-04-16 10:36:45 -0300

    Delete projetojpa01 directory

--- projetojpa01/src/main/java/com/projetojpa/repository/ProdutoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.projetojpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetojpa.entities.Produto;
-
-public interface ProdutoRepositorio extends JpaRepository<Produto, Long>{
-
-}
